34ca439cdb114a53a9a582d654687746b3e37dac
llanos1205 <llanos1205@gmail.com>
2023-10-13 23:08:05 +1300

    added more initial files

---
+++ Parking/Parking/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Parking;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Parking/Parking/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Parking;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
